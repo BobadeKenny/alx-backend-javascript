776da96698b7f470be53c34e36ea0227be19a522
Kehinde Bobade <kehinde.bobade@mpharma.com>
2022-12-21 18:58:49 +0100

    fix

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,8 @@
 export default function handleResponseFromAPI(promise) {
-  return promise.then(() => ({ status: 200, body: 'Success' })).catch((error) => {
-    console.log(error);
-  }).finally(() => {
+  return promise
+  .then(() => ({ status: 200, body: 'Success' }))
+  .catch(new Error())
+  .finally(() => {
     console.log('Got a response from the API');
   });
 }
