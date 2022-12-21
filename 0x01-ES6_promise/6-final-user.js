5c7ee021f365f3c7a2562ee3bc3b586201b78722
Kehinde Bobade <kehinde.bobade@mpharma.com>
2022-12-21 18:45:17 +0100

    eslint fixes

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,16 +1,16 @@
 import { signUpUser } from './4-user-promise';
 import { uploadPhoto } from './5-photo-reject';
 
-export function handleProfileSignup(firstName, lastName, fileName) {
+export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((values) => {
-      values.forEach((value) => {
-        if (value.status === 'rejected') {
-          value.value = `Error: ${value.reason.message}`;
-          delete value.reason;
+    .then((results) => {
+      results.forEach((result) => {
+        if (result.status === 'rejected') {
+          result.value = `Error: ${result.reason.message}`;
+          delete result.reason;
         }
-        delete value.status;
+        delete result.status;
       });
-      return values;
+      return results;
     });
 }
