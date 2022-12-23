9752f73f3ef9311c0a04bc435f0c168e958cbee0
Kehinde Bobade <kehinde.bobade@mpharma.com>
2022-12-23 05:59:32 +0100

    10

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,23 @@
+export default class Car {
+  constructor(brand, model, price) {
+    this._brand = brand;
+    this._model = model;
+    this._price = price;
+  }
+
+  get brand() {
+    return this._brand;
+  }
+
+  get model() {
+    return this._model;
+  }
+
+  get price() {
+    return this._price;
+  }
+
+  cloneCar() {
+    return new Car();
+  }
+}
