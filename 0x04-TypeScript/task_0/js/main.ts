7a66af8b07743fa4bd7908f304aaad3fd32e50f2
Kehinde Bobade <kehinde.bobade@mpharma.com>
2023-01-04 22:18:01 +0100

    task 0

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,23 @@
+interface Student {
+    firstName: string;
+    lastName: string;
+    age: number;
+    location: string;
+}
+
+let student1: Student = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 25,
+    location: "New York"
+};
+
+let student2: Student = {
+    firstName: "Jane",
+    lastName: "Doe",
+    age: 25,
+    location: "New York"
+};
+
+let studentsList = [student1, student2];
+
